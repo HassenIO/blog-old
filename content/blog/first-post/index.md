1866d02962470939a9ee951a67f2b42fb5e4b04f
Hassen Taidirt <htaidirt@gmail.com>
2019-06-02 16:38:31 +0200

    Init Gatsby blog for htaidirt.com

---
+++ content/blog/first-post/index.md
@@ -0,0 +1,8 @@
+---
+title: This is my personal test
+date: "2019-06-02T12:12:03.284Z"
+---
+
+## This is an H2 title
+
+And here we start.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `From Gatsby`,
+    author: `Hassen Taidirt`,
+    description: `Hassen Taidirt's personal blog`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `htaidirt`,
     },
   },
   plugins: [
